bb4ad48f879f75c93cbd4c321ab3a1b4b0e710d6
Akila Sadun <kumarasadun886@gmail.com>
2026-01-02 02:25:45 +0530

    update database to cloud

--- final/backend/db.js
+++ final/backend/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "ceylon_cinnamon",
+  host: "mysql-3e42a36-kumarasadun886-aff5.l.aivencloud.com",
+  user: "avnadmin",
+  password: "AVNS_UidiEFQNZqB0CY6kKrF",
+  database: "goo",
+  port: 27589,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected!");
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("MySQL Connected to Aiven Cloud Successfully!");
 });
 
 module.exports = connection;
